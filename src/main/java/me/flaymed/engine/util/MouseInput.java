6acfef203a9b8da626368c1c8a82309832b61fa0
Flaymed <mvigg8@gmail.com>
2020-07-12 15:17:16 -0400

    Removed Mouse Motion Listener
    Not needed in this class for now.

--- src/main/java/me/flaymed/engine/util/MouseInput.java
+++ src/main/java/me/flaymed/engine/util/MouseInput.java
@@ -4,10 +4,9 @@ import me.flaymed.engine.Game;
 import me.flaymed.engine.menu.Button;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.util.List;
 
-public class MouseInput implements MouseListener, MouseMotionListener {
+public class MouseInput implements MouseListener {
     @Override
     public void mouseClicked(MouseEvent mouseEvent) {
 
@@ -39,15 +38,5 @@ public class MouseInput implements MouseListener, MouseMotionListener {
     @Override
     public void mouseExited(MouseEvent mouseEvent) {
 
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent mouseEvent) {
-
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent mouseEvent) {
-        //TODO: Setup Logic for cursor hovering buildings.
     }
 }
