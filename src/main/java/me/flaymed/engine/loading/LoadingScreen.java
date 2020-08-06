ad281d26ecd0d9c39e172e56f1c37c4834e7515a
Flaymed <mvigg8@gmail.com>
2020-08-06 16:26:50 -0400

    Handle Tasks

--- src/main/java/me/flaymed/engine/loading/LoadingScreen.java
+++ src/main/java/me/flaymed/engine/loading/LoadingScreen.java
@@ -10,11 +10,36 @@ import java.util.ArrayList;
 public class LoadingScreen extends GameObject {
 
     private List<Task> tasks;
+    private int completedTasks;
 
     public LoadingScreen() {
         super(0, 0, ObjectID.Menu, true);
 
+        this.completedTasks = 0;
         this.tasks = new ArrayList<>();
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void addTask(Task task) {
+        getTasks().add(task);
+    }
+
+    public int getCompletedTasks() {
+        return completedTasks;
+    }
+
+    public int getTaskCount() {
+        return getTasks().size();
+    }
+
+    public void executeTasks() {
+        for (Task task: getTasks()) {
+            task.execute();
+            completedTasks++;
+        }
     }
 
     @Override
