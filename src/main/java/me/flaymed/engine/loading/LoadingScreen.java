8e474ec0af5ff112fd37fe044e4887d6a57b0460
Flaymed <mvigg8@gmail.com>
2020-08-06 16:08:53 -0400

    LoadingScreen Base

--- src/main/java/me/flaymed/engine/loading/LoadingScreen.java
+++ src/main/java/me/flaymed/engine/loading/LoadingScreen.java
@@ -1,4 +1,23 @@
 package me.flaymed.engine.loading;
 
-public class LoadingScreen {
+import me.flaymed.engine.handler.GameObject;
+import me.flaymed.engine.handler.ObjectID;
+import java.awt.Graphics;
+
+public class LoadingScreen extends GameObject {
+
+
+    public LoadingScreen() {
+        super(0, 0, ObjectID.Menu, true);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
 }
