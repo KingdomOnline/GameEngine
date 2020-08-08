f309c122736beaa29a4dde49de785df09be04429
Flaymed <mvigg8@gmail.com>
2020-08-08 17:35:12 -0400

    Toggle TextFields on Menus that have them

--- src/main/java/me/flaymed/engine/listeners/MenuToggleListener.java
+++ src/main/java/me/flaymed/engine/listeners/MenuToggleListener.java
@@ -3,11 +3,19 @@ package me.flaymed.engine.listeners;
 import me.flaymed.engine.event.EventHandler;
 import me.flaymed.engine.event.Listener;
 import me.flaymed.engine.event.menu.MenuToggleEvent;
+import me.flaymed.engine.menu.Menu;
+import me.flaymed.engine.menu.TextFields;
+import me.flaymed.engine.text.TextField;
 
 public class MenuToggleListener extends Listener {
 
     @EventHandler
     public void menuToggled(MenuToggleEvent e) {
-        //I mean we could figure stuff to do with this later, can always delete it.
+        Menu menu = e.getMenu();
+        if (menu instanceof TextFields) {
+            for (TextField field : ((TextFields) menu).getTextFields()) {
+                field.setShown(menu.isShown());
+            }
+        }
     }
 }
