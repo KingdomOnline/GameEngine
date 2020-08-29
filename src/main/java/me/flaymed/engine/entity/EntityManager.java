2329807775cee522473aedf0a32668c2bba5d240
Flaymed <mvigg8@gmail.com>
2020-08-29 10:58:10 -0400

    Entity List

--- src/main/java/me/flaymed/engine/entity/EntityManager.java
+++ src/main/java/me/flaymed/engine/entity/EntityManager.java
@@ -1,11 +1,29 @@
 package me.flaymed.engine.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EntityManager {
 
     private static EntityManager INSTANCE;
+    private List<LivingEntity> entities;
 
     public EntityManager() {
         INSTANCE = this;
+
+        this.entities = new ArrayList<>();
+    }
+
+    public List<LivingEntity> getEntities() {
+        return entities;
+    }
+
+    public void registerEntity(LivingEntity entity) {
+        getEntities().add(entity);
+    }
+
+    public void unregisterEntity(LivingEntity entity) {
+        getEntities().remove(entity);
     }
 
     public static EntityManager getInstance() {
